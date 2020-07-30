eed4675292410d2cb177331453e0b3f15fee2700
Harshavardhan <187y1a0575@gmail.com>
2020-07-30 21:24:31 +0530

    FIRST COMMIT

---
+++ src/components/Aloo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+function Aloo(){
+    return (
+        <p style={{color:"red"}}>Aloo</p>
+    )
+}
+export default Aloo
---
+++ src/components/Beans.js
@@ -0,0 +1,7 @@
+import React from 'react'
+function Beans(){
+    return (
+        <p style={{color:"red"}}>Beans</p>
+    )
+}
+export default Beans
