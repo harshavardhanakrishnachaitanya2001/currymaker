e27f3c25d1fddf3e478c769734ca0b1e029cab73
Harshavardhan <187y1a0575@gmail.com>
2020-07-31 07:06:47 +0530

    ADDING BEANS CURRIES TO TH LIST

--- src/components/Beans.js
+++ src/components/Beans.js
@@ -1,8 +1,24 @@
 
 import React from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 function Beans(){
+    const HamHockAndWhiteBeansStew=()=>{
+        return (
+            <div>
+                <h1 style={{color:"white"}}>Ham Hock and White Bean Stew</h1>
+                <h2 style={{color:"white"}}>Ingredients</h2>
+                <ol style={{color:"yellow"}}>
+                    <li>Hi</li>
+                </ol>
+            </div>
+        )
+    }
     return (
-        <p style={{color:"red"}}>Beans</p>
+        <Router>
+            <Link to="HamHockAndWhiteBeansStew"><button>Ham Hock and White Bean Stew</button></Link>
+            <Route path="/HamHockAndWhiteBeansStew" component={HamHockAndWhiteBeansStew}/>
+        </Router>
+    
     )
 }
 export default Beans
